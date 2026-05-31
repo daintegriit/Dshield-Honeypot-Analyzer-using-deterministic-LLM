d4062ac8ecb3f84692690b5af734dc94ff2b1eed
DAINTEGRIIT <88767330+daintegriit@users.noreply.github.com>
2026-05-30 23:21:15 -0400

    updated folders

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // This tells Tailwind to look for class usage in all files under the src directory
-    './public/index.html', // Optional: Include if you are using Tailwind classes in your HTML file
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './public/index.html', 
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#1D4ED8', // Example custom color
-        secondary: '#9333EA', // Example custom color
+        primary: '#1D4ED8', 
+        secondary: '#9333EA',
       },
-      // You can extend other properties like spacing, fonts, etc., here if needed
     },
   },
   plugins: [],
